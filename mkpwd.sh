88eafe4ff97957bf7f2aca606bd6ca6032114a62
Patrick Pedersen <ctx.xda@gmail.com>
2021-11-01 21:24:07 +0100

    mkpwd: Fixed passwords being too long
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- mkpwd.sh
+++ mkpwd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-dd if=/dev/urandom bs=48 count=1 status=none | base64 | tr +/ -_ > "$SCRIPT_DIR"/pwd
+< /dev/urandom tr -cd "[:print:]" | tr -d '\\'  | tr -d '"' | head -c 63 > "$SCRIPT_DIR"/pwd
