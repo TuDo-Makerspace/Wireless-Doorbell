1d1eb4e56b4a7a37c753cb87d7f7c889c2c2498b
Patrick Pedersen <ctx.xda@gmail.com>
2021-11-01 23:35:33 +0100

    mkpwd: Limit password to A-Z0-9
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- mkpwd.sh
+++ mkpwd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-< /dev/urandom tr -cd "[:print:]" | tr -d '\\'  | tr -d '"' | head -c 63 > "$SCRIPT_DIR"/pwd
+tr -dc A-Za-z0-9 </dev/urandom | head -c 63 > "$SCRIPT_DIR"/pwd
